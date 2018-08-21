f990254c40f29026553d270e326eb6b422db59ee
Richard Steven <richard.steven@hotmail.fr>
2018-08-21 11:08:06 +0200

    ajout des modals sur les boutton supprimer

--- web/js/customer/index.js
+++ web/js/customer/index.js
@@ -1,3 +1,13 @@
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var url = button.data('whatever'); // Extract info from data-* attributes
+    var modal = $(this);
+    modal.find('.modal-title').text('New message to ');
+    modal.find('.btnSupprimer').attr('href' , url);
+
+});
+
 function displayCustomerInfo(id){
 
     var info = document.getElementById('info'+id);
